5811c7235d79327a0cabde3ffa85a7eebdc9661f
Ryan Bridgwood <rybridgwood@gmail.com>
2024-01-15 15:20:24 -0500

    first commit

---
+++ src/main/java/frc/robot/CommandSwerveDrivetrain.java
@@ -0,0 +1,194 @@
+package frc.robot;
+
+import java.util.function.Supplier;
+
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.Utils;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest.ApplyChassisSpeeds;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.generated.TunerConstants;
+
+/**
+ * Class that extends the Phoenix SwerveDrivetrain class and implements subsystem
+ * so it can be used in command-based projects easily.
+ */
+public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsystem {
+
+//blargh
+//may need to add 
+// swerveDriveFXConfig.OpenLoopRamps.DutyCycleOpenLoopRampPeriod = Constants.Swerve.openLoopRamp;
+// swerveDriveFXConfig.OpenLoopRamps.VoltageOpenLoopRampPeriod = Constants.Swerve.openLoopRamp;
+
+
+    private static final double kSimLoopPeriod = 0.005; // 5 ms
+    private Notifier m_simNotifier = null;
+    private double m_lastSimTime;
+
+    private final SwerveRequest.ApplyChassisSpeeds autoRequest = new SwerveRequest.ApplyChassisSpeeds();   //.withDriveRequestType(DriveRequestType.Velocity);
+
+    public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, double OdometryUpdateFrequency, SwerveModuleConstants... modules) {
+        super(driveTrainConstants, OdometryUpdateFrequency, modules);
+        configurePathPlanner();
+        if (Utils.isSimulation()) {
+            startSimThread();
+        }
+    }
+    public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, SwerveModuleConstants... modules) {
+        super(driveTrainConstants, modules);
+        configurePathPlanner();
+        if (Utils.isSimulation()) {
+            startSimThread();
+        }
+    }
+
+    private void configurePathPlanner() {
+        double driveBaseRadius = 0;
+        for (var moduleLocation : m_moduleLocations) {
+            driveBaseRadius = Math.max(driveBaseRadius, moduleLocation.getNorm());
+        }
+
+        AutoBuilder.configureHolonomic(
+            ()->this.getState().Pose, // Supplier of current robot pose
+            this::seedFieldRelative,  // Consumer for seeding pose against auto
+            this::getCurrentRobotChassisSpeeds,
+            (speeds)->this.setControl(autoRequest.withSpeeds(speeds)), // Consumer of ChassisSpeeds to drive the robot
+            new HolonomicPathFollowerConfig(new PIDConstants(10, 0, 0),
+                                            new PIDConstants(10, 0, 0),
+                                            TunerConstants.kSpeedAt12VoltsMps,
+                                            driveBaseRadius,
+                                            new ReplanningConfig()),
+            ()->false, // Change this if the path needs to be flipped on red vs blue
+            this); // Subsystem for requirements
+    }
+
+    public Command applyRequest(Supplier<SwerveRequest> requestSupplier) {
+        return run(() -> this.setControl(requestSupplier.get()));
+    }
+
+    public Command getAutoPath(String pathName) {
+        return new PathPlannerAuto(pathName);
+    }
+
+    public ChassisSpeeds getCurrentRobotChassisSpeeds() {
+        return m_kinematics.toChassisSpeeds(getState().ModuleStates);
+    }
+
+    private void startSimThread() {
+        m_lastSimTime = Utils.getCurrentTimeSeconds();
+
+        /* Run simulation at a faster rate so PID gains behave more reasonably */
+        m_simNotifier = new Notifier(() -> {
+            final double currentTime = Utils.getCurrentTimeSeconds();
+            double deltaTime = currentTime - m_lastSimTime;
+            m_lastSimTime = currentTime;
+
+            /* use the measured time delta, get battery voltage from WPILib */
+            updateSimState(deltaTime, RobotController.getBatteryVoltage());
+        });
+        m_simNotifier.startPeriodic(kSimLoopPeriod);
+    }
+
+
+//May need to erase
+//This is a closed loop implementation of chassis drive for pathplanner. 
+//The open/PID version might be good enough, but this could be better if we get our sysID data 'lit'
+// blargh
+
+   /**
+     * Accepts a generic ChassisSpeeds to apply to the drivetrain.
+     */
+    public class ApplyChassisSpeedsClosed implements SwerveRequest {
+
+        /**
+         * The chassis speeds to apply to the drivetrain.
+         */
+        public ChassisSpeeds Speeds = new ChassisSpeeds();
+        /**
+         * The center of rotation to rotate around.
+         */
+        public Translation2d CenterOfRotation = new Translation2d(0, 0);
+        /**
+         * The type of control request to use for the drive motor.
+         */
+        public SwerveModule.DriveRequestType DriveRequestType = SwerveModule.DriveRequestType.Velocity;
+        /**
+         * The type of control request to use for the steer motor.
+         */
+        public SwerveModule.SteerRequestType SteerRequestType = SwerveModule.SteerRequestType.MotionMagic;
+
+        public StatusCode apply(SwerveControlRequestParameters parameters, SwerveModule... modulesToApply) {
+            var states = parameters.kinematics.toSwerveModuleStates(Speeds, CenterOfRotation);
+            for (int i = 0; i < modulesToApply.length; ++i) {
+                modulesToApply[i].apply(states[i], DriveRequestType, SteerRequestType);
+            }
+
+            return StatusCode.OK;
+        }
+
+        /**
+         * Sets the chassis speeds to apply to the drivetrain.
+         *
+         * @param speeds Chassis speeds to apply to the drivetrain
+         * @return this request
+         */
+        public ApplyChassisSpeedsClosed withSpeeds(ChassisSpeeds speeds) {
+            this.Speeds = speeds;
+            return this;
+        }
+        /**
+         * Sets the center of rotation to rotate around.
+         *
+         * @param centerOfRotation Center of rotation to rotate around
+         * @return this request
+         */
+        public ApplyChassisSpeedsClosed withCenterOfRotation(Translation2d centerOfRotation) {
+            this.CenterOfRotation = centerOfRotation;
+            return this;
+        }
+
+        /**
+         * Sets the type of control request to use for the drive motor.
+         *
+         * @param driveRequestType The type of control request to use for the drive motor
+         * @return this request
+         */
+        public ApplyChassisSpeedsClosed withDriveRequestType(SwerveModule.DriveRequestType driveRequestType) {
+            this.DriveRequestType = driveRequestType;
+            return this;
+        }
+        /**
+         * Sets the type of control request to use for the steer motor.
+         *
+         * @param steerRequestType The type of control request to use for the steer motor
+         * @return this request
+         */
+        public ApplyChassisSpeedsClosed withSteerRequestType(SwerveModule.SteerRequestType steerRequestType) {
+            this.SteerRequestType = steerRequestType;
+            return this;
+        }
+    }
+
+
+
+
+
+
+
+}
