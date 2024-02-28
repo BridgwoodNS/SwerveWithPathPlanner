674a48e8bb36dc8bd10b327c7f1e9058bcfe6a7b
bridgwoodns <bridgwoodr@northshoreschools.org>
2024-02-27 20:23:56 -0500

    added swerve stuff

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,8 +3,14 @@ package frc.robot;
 public interface Constants {
 
     int intakeMotor1 = 30;
-        int intakeMotor2 = 31;
-        int PivotMotor = 32;
+    int intakeMotor2 = 31;
 
-
+    int pivotMotor = 32;
+    
+    int intakefrontId = 11;
+    int intakebackId = 12;
+    int elevatorleftId = 8;
+    int elevatorrightId = 9;
+    int shootertopId = 13;
+    int shooterbottomId = 14;
 }
