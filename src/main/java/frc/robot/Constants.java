dbfb970e0a3f9210d0faa3871515a4d7ddbae2c3
bridgwoodns <bridgwoodr@northshoreschools.org>
2024-02-28 15:37:21 -0500

    Revert "added swerve stuff"
    This reverts commit 674a48e8bb36dc8bd10b327c7f1e9058bcfe6a7b.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,14 +3,8 @@ package frc.robot;
 public interface Constants {
 
     int intakeMotor1 = 30;
-    int intakeMotor2 = 31;
+        int intakeMotor2 = 31;
+        int PivotMotor = 32;
 
-    int pivotMotor = 32;
-    
-    int intakefrontId = 11;
-    int intakebackId = 12;
-    int elevatorleftId = 8;
-    int elevatorrightId = 9;
-    int shootertopId = 13;
-    int shooterbottomId = 14;
+
 }
